27571c6340af836aa48a993051a20ae943ae4679
Brenden <brendenlouis5@gmail.com>
2024-07-15 19:08:23 -0400

    Init commit

---
+++ src/grid.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <raylib.h>
+#include <iostream>
+
+class Grid 
+{
+public:
+    Grid();
+    void Initialize();
+    void Print();
+    void Draw();
+    bool isCellOutside(int row, int column);
+    bool isCellEmpty(int row, int column);
+    int grid[20][10];
+private:
+    int numRows;
+    int numCols;
+    int cellSize;
+    std::vector<Color> colors;
+
+};
